0dcf80639e8a3cb1318358aa7553408e5275983d
Carlos Henrique Gomes <carlos.hgs@outlook.com.br>
2021-08-02 20:33:11 -0300

    version 1.1.2

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,20 @@
+require('dotenv').config();
 
+const database = process.env.DB_NAME;
+const user = process.env.DB_USER;
+const password = process.env.DB_PASS;
+const host = process.env.DB_HOST;
+const port = process.env.DB_PORT;
 
 module.exports = {
     client: 'mysql',
     connection:  {
-      database: 'tasks',
-      user: 'root',
-      password: 'root'
+      host,
+      user,
+      port,
+      password,
+      database
+      
     },
     pool: {
       min: 2,
